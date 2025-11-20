4cb536d9487422fc9d62b341329064b37bcbd2e3
Roger Calaf <rcalaf@Mac.fritz.box>
2025-11-20 22:41:59 +0100

    ready for release

--- src/components/ObservationItem.js
+++ src/components/ObservationItem.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Image, Linking, TouchableOpacity } from 'react-
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import moment from 'moment';
 import { useTranslation } from "react-i18next";
+import { useMomentLocale } from "../hooks/useMomentLocale";
 
 import {ObservationContext } from '../context/ObservationContext';
 
@@ -17,6 +18,7 @@ import {ObservationContext } from '../context/ObservationContext';
 
 export default function Item({ item, index,  navigation  }) {
     const { t } = useTranslation();
+    const momentLocale = useMomentLocale();
     const {setSelectedIndex, updateSelectedIndex} = useContext(ObservationContext);
     // console.log(item);
 
@@ -88,7 +90,8 @@ export default function Item({ item, index,  navigation  }) {
           <View style={{marginTop:5,marginBottom: (item.status > 0 ? 5 : 0), alignItems:"flex-start",flex:1}}>
             {/* <Text style={{fontWeight:"bold"}}>{item.title}</Text> */}
             {title(item.title)}
-            <Text style={{ marginTop: (item.status > 0 ? 5 : 0)}}>{moment(item.date).format('Do MMMM YYYY')}</Text>
+            <Text style={{ marginTop: (item.status > 0 ? 5 : 0)}}>{moment(item.date).locale(momentLocale).format('Do MMMM YYYY')}</Text>
+           
           </View>
           {button()}
         </View>
